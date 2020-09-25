90174454e29047b101081c666b1b8eb4014e438f
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-09-25 10:02:25 +0300

    build fixed

--- bin/build.js
+++ bin/build.js
@@ -1,12 +1,11 @@
 const DOSSIER_SEED_FILE_PATH = "./seed";
 const CARDINAL_SEED_FILE_PATH = "../cardinal/seed";
 const THEMES_PATH = "../themes";
-const CONFIG_PATH = "../code/config.json";
 const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://localhost:8080";
 const DEFAULT_DOMAIN = "default";
 
-require("./../../../privatesky/psknode/bundles/csbBoot.js");
-require("./../../../privatesky/psknode/bundles/openDSU.js");
+require("./../../privatesky/psknode/bundles/csbBoot.js");
+require("./../../privatesky/psknode/bundles/openDSU.js");
 
 const fs = require("fs");
 const openDSU = require("opendsu");
@@ -17,8 +16,6 @@ bdns.addRawInfo(DEFAULT_DOMAIN, {
     brickStorages: [BRICK_STORAGE_ENDPOINT],
     anchoringServices: [BRICK_STORAGE_ENDPOINT]
 })
-
-let APP_CONFIG = {};
 
 function getCardinalDossierSeed(callback){
     fs.readFile(CARDINAL_SEED_FILE_PATH, (err, content)=>{
